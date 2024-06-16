a337d441cfecb67f1caf44c0b976fa0acb5e3d0b
seohyun0120 <sarahseohyun@gmail.com>
2024-06-16 23:35:55 +0900

    feat: 테스트 시작 전후 mongo 서버 처리

---
+++ test/setup.ts
@@ -0,0 +1,15 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+
+let mongoServer: MongoMemoryServer;
+
+beforeAll(async () => {
+  mongoServer = await MongoMemoryServer.create();
+  const uri = mongoServer.getUri();
+  await mongoose.connect(uri);
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+  await mongoServer.stop();
+});
