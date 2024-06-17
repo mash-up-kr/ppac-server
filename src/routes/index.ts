90648ff8d38745f27b8943d4f2a4fa20752922f7
pjw5521 <pjw5521@naver.com>
2024-06-18 00:28:34 +0900

    feat: add router config

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 
 import meme from './meme';
 import user from './user';
+import keyword from './keyword';
 import { errorHandler } from '../middleware/errorHandler';
 import { loggerMiddleware } from '../util/logger';
 
@@ -11,6 +12,7 @@ router.use(loggerMiddleware);
 
 router.use('/api/meme', meme);
 router.use('/api/user', user);
+router.use('/api/keyword', keyword);
 
 router.use(errorHandler);
 
