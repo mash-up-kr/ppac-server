3ccd2315a92aec526cbd8eb1a7ecf1f923312b13
pjw5521 <pjw5521@naver.com>
2024-06-29 15:57:47 +0900

    feat: add keywordCategory api

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import meme from './meme';
 import user from './user';
 import keyword from './keyword';
+import keywordCategory from './keywordCategory';
 import { errorHandler } from '../middleware/errorHandler';
 import { loggerMiddleware } from '../util/logger';
 
@@ -13,6 +14,7 @@ router.use(loggerMiddleware);
 router.use('/api/meme', meme);
 router.use('/api/user', user);
 router.use('/api/keyword', keyword);
+router.use('/api/keywordCategory', keywordCategory);
 
 router.use(errorHandler);
 
