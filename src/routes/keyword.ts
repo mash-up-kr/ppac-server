a520037341cfc5808e428743897a4b544e2e7786
seohyun0120 <sarahseohyun@gmail.com>
2024-06-29 16:06:35 +0900

    feat: 주석 추가

--- src/routes/keyword.ts
+++ src/routes/keyword.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+
 import {
   createKeyword,
   getTopKeywords,
@@ -13,11 +14,11 @@ const router = express.Router();
 
 router.use(loggerMiddleware);
 
-router.post('/', createKeyword);
-router.put('/:keywordId', getKeywordInfoById, updateKeyword);
-router.delete('/:keywordId', getKeywordInfoById, deleteKeyword);
+router.post('/', createKeyword); // 키워드 생성
+router.put('/:keywordId', getKeywordInfoById, updateKeyword); // 키워드 수정
+router.delete('/:keywordId', getKeywordInfoById, deleteKeyword); // 키워드 삭제
 
-router.get('/top', getTopKeywords);
-router.post('/increaseSearchCount', getKeywordInfoByName, increaseSearchCount);
+router.get('/top', getTopKeywords); // 인기 키워드 조회
+router.patch('/count', getKeywordInfoByName, increaseSearchCount); // 키워드 조회수 업데이트
 
 export default router;
